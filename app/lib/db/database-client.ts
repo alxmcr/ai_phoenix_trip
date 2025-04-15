f3bbc727236101ea53dd8c4a48696d59b677ba78
Alejandro Coca <alxmcr@outlook.com>
2025-04-15 19:48:23 -0400

    chore: remove console.log

--- app/lib/db/database-client.ts
+++ app/lib/db/database-client.ts
@@ -1,17 +1,13 @@
 import '@/envConfig.ts'
 import postgres from 'postgres';
-console.log("🚀 ~ process.env.NODE_ENV:", process.env.NODE_ENV)
 
 const env = process.env.NODE_ENV || 'development';
 
 const isProduction = env === 'production';
-console.log("\n\n🚀 ~ isProduction:", isProduction)
 
 const connectionString = isProduction
   ? process.env.NEON_DATABASE_URL
   : process.env.SUPABASE_DATABASE_URL;
-
-console.log("🚀 ~ connectionString:", connectionString)
 
 if (!connectionString) {
   throw new Error('DATABASE_URL environment variable is not set');
