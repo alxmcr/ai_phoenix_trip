75bb123302741215f7fff701c88affd66dd4c615
Alejandro Coca <alxmcr@outlook.com>
2025-04-15 19:01:57 -0400

    chore: create database client

---
+++ app/lib/db/database-client.ts
@@ -0,0 +1,20 @@
+import './envConfig.ts'
+import postgres from 'postgres';
+
+const env = process.env.NODE_ENV || 'development';
+
+const isProduction = env === 'production';
+console.log("\n\n🚀 ~ isProduction:", isProduction)
+
+const connectionString = isProduction
+  ? process.env.NEON_DATABASE_URL
+  : process.env.SUPABASE_DATABASE_URL;
+
+console.log("🚀 ~ connectionString:", connectionString)
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const sql = postgres(connectionString);
+export default sql;
