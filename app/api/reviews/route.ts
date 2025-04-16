682722d7c1ebdae9351978fcbe449ff55806d33a
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-16 01:22:29 -0400

    feat: create reviews endpoints (#11)

---
+++ app/api/reviews/route.ts
@@ -0,0 +1,61 @@
+// POST request: create an Review
+
+import { ReviewDB } from "@/app/classes/db/review-db";
+import { HTTPResponseCode } from "@/app/enums/http-response-code";
+
+const dbReview = new ReviewDB();
+
+export async function GET(request: Request) {
+  // get the page and limit from the request
+  const { searchParams } = new URL(request.url);
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = Number(searchParams.get("limit")) || 10;
+
+  const Reviews = await dbReview.paginate(page, limit);
+
+  return new Response(JSON.stringify(Reviews), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+export async function POST(request: Request) {
+  // Parse the request body
+  const body = await request.json();
+
+  try {
+    //Insert new Review into your DB
+    const newReview = await dbReview.create(body);
+
+    // check if newAction is not null or undefined
+    if (!newReview) {
+      return new Response(
+        JSON.stringify({ error: "Failed to create Review" }),
+        {
+          status: HTTPResponseCode.BAD_REQUEST,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+
+    // Return the new Review
+    return new Response(JSON.stringify(newReview), {
+      status: HTTPResponseCode.CREATED,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: HTTPResponseCode.BAD_REQUEST,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return new Response(
+      JSON.stringify({ error: "An unknown error occurred" }),
+      {
+        status: HTTPResponseCode.INTERNAL_SERVER_ERROR,
+      }
+    );
+  }
+}
