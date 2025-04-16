cc45b66a8d67f2cc15ee692de2782202e555298e
Alejandro Coca <alxmcr@outlook.com>
2025-04-16 02:19:02 -0400

    chore: create classes to manager reviews

--- app/types/ai/review-ai-response.ts
+++ app/types/ai/review-ai-response.ts
@@ -2,10 +2,10 @@ import { ActionableData } from "../db/actionable";
 import { RecommendationData } from "../db/recommendation";
 import { SentimentData } from "../db/sentiment";
 
-export interface ReviewAIResponse {
+export type ReviewAIResponse = {
   review_id: string;
   actionable: ActionableData[];
   sentiment: SentimentData[];
   recommendation: RecommendationData[];
-}
+};
 
