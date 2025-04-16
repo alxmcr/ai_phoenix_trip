9f98a2a4f2fdaf552646f1331f5b73660ba27a7a
Alejandro Coca <alxmcr@outlook.com>
2025-04-16 02:28:21 -0400

    fix: add missing call to analyzeReview function

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,4 +1,5 @@
 import { ReviewDB } from "@/app/classes/db/review-db";
+import { ManagerOpenAI } from "@/app/classes/open-ai/manager-open-ai";
 import { HTTPResponseCode } from "@/app/enums/http-response-code";
 import { validate as validateUUID } from "uuid";
 
@@ -47,8 +48,11 @@ export async function POST(request: Request) {
       });
     }
 
+    // Analyze the review with OpenAI
+    const reviewAIResponse = await ManagerOpenAI.analyzeReview(newReview);
+
     // Return the new Review
-    return new Response(JSON.stringify(newReview), {
+    return new Response(JSON.stringify(reviewAIResponse), {
       status: HTTPResponseCode.CREATED,
       headers: { "Content-Type": "application/json" },
     });
