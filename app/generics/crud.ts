d00fba41579258e04f39720c8ec4578a8d8d6fc5
Alejandro Coca <alxmcr@outlook.com>
2025-04-15 22:03:18 -0400

    chore: create app generics

---
+++ app/generics/crud.ts
@@ -0,0 +1,6 @@
+export interface CRUD<T> {
+  create(item: T): Promise<T>;
+  read(id: string): Promise<T | null>;
+  update(id: string, item: Partial<T>): Promise<T | null>;
+  delete(id: string): Promise<boolean>;
+}
