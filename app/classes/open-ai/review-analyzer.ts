df6a28d734e466799403f4dbbd0c95d818838aed
Alejandro Coca <alxmcr@outlook.com>
2025-04-16 22:23:09 -0400

    chore: update prompt review

--- app/classes/open-ai/review-analyzer.ts
+++ app/classes/open-ai/review-analyzer.ts
@@ -3,36 +3,56 @@ import { TRAVEL_ASSISTANT_REVIEW_ANALYZER } from "@/app/config/ai/openai-system-
 import { OpenAIMessageRoles } from "@/app/enums/openai-message-roles";
 import { OpenAIModelsCostOptimized } from "@/app/enums/openai-models";
 import { OpenAITemperatures } from "@/app/enums/openai-temperatures";
+import { buildPromptReview } from "@/app/helpers/ai/prompt-review";
+import { ReviewData } from "@/app/types/db/review";
 
 export class ReviewAnalyzer {
   private model: OpenAIModelsCostOptimized;
   private temperature: OpenAITemperatures;
   private directiveSystem: string;
+  private maxTokens: number;
 
   constructor() {
     this.model = OpenAIModelsCostOptimized.GPT_3_5_TURBO;
     this.temperature = OpenAITemperatures.BALANCED_CREATIVE;
     this.directiveSystem = TRAVEL_ASSISTANT_REVIEW_ANALYZER;
+    this.maxTokens = 2000;
   }
 
   // Use the Responses API from OpenAI to analyze the review
   // Source: https://platform.openai.com/docs/api-reference/responses/create
   // Roles: system, user
-  async analyzeReview(review: string) {
-    console.log("🚀 ~ ReviewAnalyzer ~ analyzeReview ~ review:", review)
-    const prompt = "";
+  async analyzeReview(review: ReviewData) {
+    const prompt = buildPromptReview(review);
 
-    const response = await openai.responses.create({
-      temperature: this.temperature,
+    // Create a response from OpenAI
+    const response = await openai.chat.completions.create({
       model: this.model,
-      input: [
+      store: false,
+      temperature: this.temperature,
+      max_tokens: this.maxTokens,
+      response_format: { type: "json_object" },
+      messages: [
         {
           role: OpenAIMessageRoles.SYSTEM,
           content: this.directiveSystem,
         },
-        { role: OpenAIMessageRoles.USER, content: prompt },
+        {
+          role: OpenAIMessageRoles.USER,
+          content: prompt,
+        },
       ],
     });
-    console.log("🚀 ~ ReviewAnalyzer ~ analyzeReview ~ response:", response)
+
+    // Print the response
+    console.log("🚀 ~ ReviewAnalyzer ~ analyzeReview ~ response:", response);
+
+    // Printy typeof response
+    console.log(
+      "🚀 ~ ReviewAnalyzer ~ analyzeReview ~ typeof response:",
+      typeof response
+    );
+
+    return response;
   }
 }
