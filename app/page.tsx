bc951483240f279e58f737b4c7497931b7930290
Alejandro Coca <alxmcr@outlook.com>
2025-04-17 16:13:44 -0400

    chore: create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/sections/footer";
+import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen">
+    <main className="flex flex-col min-h-screen px-4 md:px-0 items-center">
+      <Hero />
       <Footer />
     </main>
   );
