c0b1c2f0e5c6826ab9721a1546d93b9a21ccb3dd
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-17 19:46:18 -0400

    chore: review-page-hero-section (#35)
    * chore: create hero section on review page
    
    * chore: remove console.log

--- app/types/db/review.ts
+++ app/types/db/review.ts
@@ -1,8 +1,8 @@
 export interface ReviewData {
   review_id: string;
   rating: number;
-  start_date: Date; // ISO format date
-  end_date: Date; // ISO format date
+  start_date: string; // ISO format date
+  end_date: string; // ISO format date
   created_at: string; // ISO format timestamp
   updated_at: string; // ISO format timestamp
   destination: string;
