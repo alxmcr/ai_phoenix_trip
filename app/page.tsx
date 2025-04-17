73a13f7714972a84cb70b1389535ded73059a71c
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-17 16:38:01 -0400

    chore: partners-section (#29)
    * chore: refactor hero and footer sections
    
    * chore: create partners section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/sections/footer";
 import Hero from "@/components/sections/hero";
+import Partners from "@/components/sections/partners";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen px-4 md:px-0 items-center">
       <Hero />
+      <Partners />
       <Footer />
     </main>
   );
