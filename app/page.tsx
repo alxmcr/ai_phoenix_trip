077222204437809eeae322411a2ff32a5995d720
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-17 19:07:16 -0400

    refactor: sections-page (#34)
    * refactor: sections page
    
    * 0.0.4

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Features from "@/components/sections/features";
+import Features from "@/components/sections/landing-page/features";
 import Footer from "@/components/sections/footer";
-import Hero from "@/components/sections/hero";
-import HowItWorks from "@/components/sections/how-it-works";
-import Partners from "@/components/sections/partners";
-import ShareTravelExperience from "@/components/sections/share-travel-experience";
+import Hero from "@/components/sections/landing-page/hero";
+import HowItWorks from "@/components/sections/landing-page/how-it-works";
+import Partners from "@/components/sections/landing-page/partners";
+import ShareTravelExperience from "@/components/sections/landing-page/share-travel-experience";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center">
