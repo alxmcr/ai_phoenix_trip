79f0c59ead8cdb9052df54ba1544d364cbe939e9
Alejandro Coca <alxmcr@outlook.com>
2025-04-17 14:22:18 -0400

    fix: header details

--- components/header.tsx
+++ components/header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="text-2xl font-bold bg-gradient-to-r from-purple-600 to-blue-500 text-transparent bg-clip-text">
+            <span className="px-2 text-2xl font-bold bg-gradient-to-r from-purple-600 to-blue-500 text-transparent bg-clip-text">
               Phoenix Trip
             </span>
           </Link>
