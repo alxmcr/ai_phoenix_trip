d2f4b4573dae9b0de0d8ba6f7acbd3d693f069b5
Alejandro Coca <alxmcr@outlook.com>
2025-04-17 22:05:42 -0400

    chore: create 'Insights & Actions' section

--- app/reviews/[id]/page.tsx
+++ app/reviews/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { ReviewAIResponse } from "@/app/types/ai/review-ai-response";
 import Footer from "@/components/sections/footer";
 import { Hero } from "@/components/sections/review-page/hero";
+import { InsightsTabs } from "@/components/sections/review-page/insights-tabs";
 import { Sentiment } from "@/components/sections/review-page/sentiment-section";
 import HeroSkeleton from "@/components/skeletons/hero-skeleton";
+import InsightsSkeleton from "@/components/skeletons/insights-skeleton";
 import { SentimentSkeleton } from "@/components/skeletons/sentiment-skeleton";
 import { Suspense } from "react";
 
@@ -39,6 +41,18 @@ export default async function ReviewPage({
       <Suspense fallback={<SentimentSkeleton />}>
         <Sentiment sentiment={reviewAnalyzed.sentiment} />
       </Suspense>
+
+      <section className="mb-8 px-4 md:px-0 container">
+        <h2 className="text-2xl font-bold tracking-tight mb-4">
+          Insights & Actions
+        </h2>
+        <Suspense fallback={<InsightsSkeleton />}>
+          <InsightsTabs
+            actionables={reviewAnalyzed.actionables}
+            recommendations={reviewAnalyzed.recommendations}
+          />
+        </Suspense>
+      </section>
       <Footer />
     </main>
   );
