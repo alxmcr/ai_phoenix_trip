b257244beac9f76ea3db635e42a9f1ffb8c0a2cc
Alejandro M. Coca <alxmcr@users.noreply.github.com>
2025-04-23 03:11:21 -0400

    fix: building-issues (#47)
    * fix: build issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
+import Header from "@/components/sections/header";
+import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import Header from "@/components/sections/header";
-import Head from "next/head";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,18 +15,47 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Phoenix Trip - AI-powered insights from passenger trip experiences",
-  description:
-    "Get AI-powered insights from passenger trip experiences with sentiment analysis, actionable recommendations, and more.",
-  openGraph: {
-    title: "Phoenix Trip - AI-powered insights from passenger trip experiences",
-    description:
-      "Get AI-powered insights from passenger trip experiences with sentiment analysis, actionable recommendations, and more.",
-    images: [{ url: "/og-image.png" }],
+  title: {
+    template: "%s | Phoenix Trip",
+    default: "Phoenix Trip",
   },
   icons: {
     icon: "/logo-phoenix-trip-ai-16x16.svg",
     apple: "/logo-phoenix-trip-ai-16x16.svg",
+  },
+  openGraph: {
+    title: "Phoenix Trip - AI-powered insights from passenger trip experiences",
+    description:
+      "Get AI-powered insights from passenger trip experiences with sentiment analysis, actionable recommendations, and more.",
+    url: "https://ai-phoenix-trip.vercel.app",
+    siteName: "Phoenix Trip",
+    images: [
+      {
+        url: "https://ai-phoenix-trip.vercel.app/og.png", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://ai-phoenix-trip.vercel.app/og-alt.png", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    videos: [
+      {
+        url: "https://ai-phoenix-trip.vercel.app/video.mp4", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    audio: [
+      {
+        url: "https://nextjs.org/audio.mp3", // Must be an absolute URL
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
