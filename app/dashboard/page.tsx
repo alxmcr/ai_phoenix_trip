2140572a044b4b73dee547f481c068a1197f11cc
Alejandro Coca <alxmcr@outlook.com>
2025-04-23 01:27:14 -0400

    chore: update dependencies

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -81,6 +81,7 @@ export default async function DashboardPage() {
           <ActionableInsights />
           <TopRecommendations />
         </div>
+        <RecentReviews />
       </Suspense>
     </main>
   );
