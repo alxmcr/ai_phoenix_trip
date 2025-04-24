a0175a24cc18d5b855ecf7efe5129d4a29c3968f
Alejandro Coca <alxmcr@outlook.com>
2025-04-24 18:10:48 -0400

    chore: fix openai api key issues

--- app/config/ai/openai-client.ts
+++ app/config/ai/openai-client.ts
@@ -1,5 +1,18 @@
+import "@/envConfig.ts";
 import { OpenAI } from "openai";
 
+const env = process.env.NODE_ENV || "development";
+
+const isProduction = env === "production";
+
+const openai_key = isProduction
+  ? process.env.OPENAI_API_KEY
+  : process.env.OPENAI_API_KEY_DEV;
+
+if (!openai_key) {
+  throw new Error("OPENAI_API_KEY is not set");
+}
+
 export const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: openai_key,
 });
