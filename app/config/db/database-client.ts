83e5c47d31324843c60109f1bab797d93fd0c077
Alejandro Coca <alxmcr@outlook.com>
2025-04-24 01:39:10 -0400

    chore: improvements database client

--- app/config/db/database-client.ts
+++ app/config/db/database-client.ts
@@ -7,7 +7,7 @@ const isProduction = env === "production";
 
 const connectionString = isProduction
   ? process.env.NEON_DATABASE_URL
-  : process.env.SUPABASE_DATABASE_URL;
+  : process.env.LOCAL_DATABASE_URL;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
